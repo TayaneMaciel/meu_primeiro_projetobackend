bf07eb131f58fee60af8d961117a15a2f573aab7
TayaneMaciel <tayanemaciel.abreu@gmail.com>
2023-05-29 22:53:13 -0300

    atualizei mulher.js

--- mulher.js
+++ mulher.js
@@ -1,21 +1,22 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
 
-const app = express()
-const porta = 3333
+const router = express.Router();
+const app = express();
 
+const porta = 3333;
 
 function mostraMulher(request, response) {
-response.json({
-    nome: 'Tayane Abreu',
-    imagem: 'https://pbs.twimg.com/profile_images/1609742365064839172/16V1PY3o_400x400.jpg',
-    minibio: 'Desenvolvedora em nuvem' 
-})
+  response.json({
+    nome: "Tayane Abreu",
+    imagem:
+      "https://pbs.twimg.com/profile_images/1609742365064839172/16V1PY3o_400x400.jpg",
+    minibio: "Desenvolvedora em nuvem",
+  });
 }
 
-function mostraPorta(){
-console.log("Servidor criado e rodando na porta", porta)
+function mostraPorta() {
+  console.log("Servidor criado e rodando na porta", porta);
 }
 
-app.use(router.get('/mulher', mostraMulher))
-app.listen(porta, mostraPorta)
+app.use(router.get("/mulher", mostraMulher));
+app.listen(porta, mostraPorta);
