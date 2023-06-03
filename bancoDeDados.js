3bb632e1f16f9f92f334a9bd6a76bd157c803083
TayaneMaciel <tayanemaciel.abreu@gmail.com>
2023-06-03 20:51:46 -0300

    projeto pronto

---
+++ bancoDeDados.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function conectaBancoDeDados() {
+  try {
+    console.log("conexão com banco de dados iniciou");
+
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log("conexão com banco de dados feita com sucesso");
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+module.exports = conectaBancoDeDados;
--- mulheres.js
+++ mulheres.js
@@ -1,78 +1,89 @@
 const express = require("express"); //aqui estou iniciando o express
 const router = express.Router(); // aqui estou configurando a primeira parte da rota
-const { v4: uuidv4 } = require('uuid');
+const cors = require("cors"); // aqui estou trazendo o pacote CORS que permite consumir essa API no front end
+
+const conectaBancoDeDados = require("./bancoDeDados"); //aqui estou ligando ao arquivo banco de dados
+conectaBancoDeDados(); //estou chamando a função que convecta o banco de dados
+
+const Mulher = require("./mulherModel");
 
 const app = express(); // aqui estou iniciando o app
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 const porta = 3333; // aqui estou criando a porta
 
-//aqui estou criando lista inicial de mulheres
-const mulheres= [
-    {
-        id: '1',
-        nome: 'Tayane Maciel',
-        imagem: 'https://pbs.twimg.com/profile_images/1609742365064839172/16V1PY3o_400x400.jpg',
-        minibio: 'desenvolvedora em nuvem'
-    },
-    {
-        id:'2',
-        nome: 'Rosana Maciel',
-        imagem: 'https://pbs.twimg.com/profile_images/1609742365064839172/16V1PY3o_400x400.jpg',
-        minibio: 'desenvolvedora front end'   
-    }
-]
-
 //GET
-function mostraMulheres(request, response) {
-    response.json(mulheres)
+async function mostraMulheres(request, response) {
+  try {
+    const mulheresVindasDoBancoDeDados = await Mulher.find();
+
+    response.json(mulheresVindasDoBancoDeDados);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
 //POST
-function criaMulher(request, response){
-    const novaMulher= {
-        id: uuidv4(),
-        nome: request.body.nome,
-        imagem: request.body.imagem,
-        minibio: request.body.minibio
-    }
-
-    mulheres.push(novaMulher)
-    response.json(mulheres)
+async function criaMulher(request, response) {
+  const novaMulher = new Mulher({
+    nome: request.body.nome,
+    imagem: request.body.imagem,
+    minibio: request.body.minibio,
+    citacao: request.body.citacao,
+  });
+  try {
+    const mulherCriada = await novaMulher.save();
+    response.status(201).json(mulherCriada);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
 //PATCH
-function corrigeMulher(request,response){
-    function encontraMulher(mulher){
-        if (mulher.id===request.params.id){
-            return mulher
-        }
-    }
-    const mulherEncontrada = mulheres.find(encontraMulher)
+async function corrigeMulher(request, response) {
+  try {
+    const mulherEncontrada = await Mulher.findById(request.params.id);
 
-    if (request.body.name){
-        mulherEncontrada.nome = request.body.nome
+    if (request.body.nome) {
+      mulherEncontrada.nome = request.body.nome;
     }
 
-    if (request.body.minibio){
-        mulherEncontrada.minibio = request.body.minibio
+    if (request.body.minibio) {
+      mulherEncontrada.minibio = request.body.minibio;
     }
 
-    if (request.body.imagem){
-        mulherEncontrada = request.body.imagem
+    if (request.body.imagem) {
+      mulherEncontrada = request.body.imagem;
+    }
+    if (request.body.citacao) {
+      mulherEncontrada = request.body.citacao;
     }
 
-    response.json(mulheres)
+    const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save();
+    response.json(mulherAtualizadaNoBancoDeDados);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
-
+//DELETE
+async function deletaMulher(request, response) {
+  try {
+    await Mulher.findByIdAndDelete(request.params.id);
+    response.json({ mensagem: "mulher deletada com sucesso!" });
+  } catch (erro) {
+    console.log(erro);
+  }
+}
 
 app.use(router.get("/mulheres", mostraMulheres)); // configuração rota GET /mulheres
-app.use(router.post('/mulheres', criaMulher)); // cinfigurei rota POST /mulheres
-app.use(router.patch('/mulheres/:id', corrigeMulher)) // configurei a rota patch /mulheres/:id
+app.use(router.post("/mulheres", criaMulher)); // cinfigurei rota POST /mulheres
+app.use(router.patch("/mulheres/:id", corrigeMulher)); // configurei a rota patch /mulheres/:id
+app.use(router.delete("/mulheres/:id", deletaMulher)); // configurei a rota DELETE/ mulheres/:id
 
 //PORTA
 function mostraPorta() {
-    console.log("Servidor criado e rodando na porta", porta);
-  }
+  console.log("Servidor criado e rodando na porta", porta);
+}
 
 app.listen(porta, mostraPorta); //servidor ouvindo a porta
